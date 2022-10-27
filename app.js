1531a355db78d54ab3a916f94f2f2b30c2a6de20
Daniel Cruz <danielcruz906@gmail.com>
2022-10-27 11:21:53 -0300

    exemplos das primeiras aulas sobre fundamentos

---
+++ app.js
@@ -0,0 +1,34 @@
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacao = primeiroNumero * segundoNumero;
+
+// console.log(operacao);
+
+const idade = 24;
+const pi = Math.PI;
+
+// console.log(idade / pi);
+
+const a = 10;
+const b = 0;
+
+// console.log(a / b);
+// console.log(b / a);
+// console.log(b / b);
+
+const hiragana = '\u3041';
+// console.log(hiragana);
+
+const portoAlegre = 'porto alegre';
+const input = 'Porto Alegre';
+const inputMinusculo = input.toLowerCase();
+
+// console.log(portoAlegre==inputMinusculo);
+
+// //BOOLEANS
+
+const firstNumber = 5;
+const secondNumber = 10;
+
+console.log(firstNumber===secondNumber/2)
