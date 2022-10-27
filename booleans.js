1ae0a5f6e27c4bd70a972b121492d59590cb2bdd
Daniel Cruz <danielcruz906@gmail.com>
2022-10-27 17:21:07 -0300

    add booleans

---
+++ booleans.js
@@ -0,0 +1,25 @@
+// // BOOLEANS
+
+ const usuarioLogado = true
+ const contaPaga = false
+
+ // // TRUTHY & FALSY
+
+ // 0 => falsy
+ // 1 => truthy (para algumas linguagens 0 e 1) podem ter valor de verdadeiro e falso
+
+//  console.log (0 == false);
+//  console.log ("" == false);
+//  console.log (1 == true);
+
+ let minhaVar;
+ let meuNull = null;
+
+//  console.log (minhaVar);
+//  console.log (meuNull);
+
+ let numero = 5
+ let texto = 'Meu texto'
+
+ console.log (typeof numero);
+ console.log (typeof texto);
