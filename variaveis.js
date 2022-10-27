48352f09568bca8b51cfa46007c1b04563360446
Daniel Cruz <danielcruz906@gmail.com>
2022-10-27 16:58:35 -0300

    estudo de variaveis

---
+++ variaveis.js
@@ -0,0 +1,37 @@
+// // VAR -- variavel bem liberal e quase nao usada hoje em dia pela frequencia de bugs.
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+
+// // LET
+
+// let forma = 'retangulo'
+// let altura = 5;
+// let comprimento = 7;
+
+// if (forma === 'retngulo'){
+//     area = altura * comprimento;
+// } else {
+//     area = (altura * comprimento) / 2;
+// }
+
+// console.log(area)
+// let = area;
+
+// // CONST -- const tem um valor constante. uma vez declarada seu valor nao muda.
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 5;
+let area; 
+
+if (forma ==='quadrado'){
+     area = altura * comprimento;
+} else{
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
