91b3ccdbce76e0559a512f41758f789139cbc5c8
Daniel Cruz <danielcruz906@gmail.com>
2022-11-03 23:41:16 -0300

    operadores e funções

---
+++ expressoes-funcao.js
@@ -0,0 +1,21 @@
+// // forma classica de escrever funcao:
+
+// function minhaFuncao(param){
+//     //bloco de codigo
+// }
+
+// minhaFuncao(param);
+
+// // expressao de funcao (outra forma de escrever funcao em Js)
+
+const soma = function (num1, num2){
+    return num1 + num2;
+}
+
+console.log(soma(1,1));
+
+// diferença entre declaração e expressão de função
+
+function apresentar(){
+    return "Olá!"
+}
